015144e52e7deb4bbcd30a29d3193849d85eb791
Nicholas Vachon MBANV <nicholasvachon@hotmail.com>
2025-08-04 13:50:53 -0400

    logging.py to logs.py to avoid confusion with python package

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/utils.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/utils.py
@@ -1,6 +1,6 @@
 import logging
 
-from {{cookiecutter.package_name}}.config.logging import setup_logging
+from {{cookiecutter.package_name}}.config.logs import setup_logging
 
 setup_logging()
 logger = logging.getLogger(__name__)
